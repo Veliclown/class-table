c89d27ba749380962695de4b0cc3cf6463048af0
Veliclown <kukeruluukr@gmail.com>
2022-11-22 23:41:18 +0200

    add new branch

---
+++ stud/dileron.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream> 
+#include <iomanip> 
+#include "windows.h"
+#include "time.h"
+
+using namespace std;
+
+int main()
+{
+	const int r = 4;
+	const int st = 4;
+
+	int b[r][st] = { {-2,3,4,5},{2,5,6,7},{-3,-4,-5,-6},{2,4,5,6} };
+
+	for (int i = 0; i < st; i++)
+	{
+		cout << setw(5) << b[i][i] << endl;
+	}
+
+	int min = b[0][0];
+	for (int i = 0; i < st; i++)
+		for (int i = 0; i < r; i++)
+			if (b[i][i] < min) min = b[i][i];
+
+	cout << min;
+
+
+}
